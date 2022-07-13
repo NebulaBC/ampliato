e0b3166e6515a89705dd2432c791c3152023b098
NebulaBC <contact@nebulabc.net>
2022-07-13 17:03:14 -0400

    Update entries

--- ampliato.py
+++ ampliato.py
@@ -1,6 +1,12 @@
 import keyboard
 
-keyboard.add_abbreviation(';p', '\N{rolling on the floor laughing}')
+keyboard.add_abbreviation(';r;', '🤣')
+keyboard.add_abbreviation(';p;', '😛')
+keyboard.add_abbreviation(';s;', '😀')
+keyboard.add_abbreviation(';z;', '🤪')
+keyboard.add_abbreviation(';u;', '👍')
+keyboard.add_abbreviation(';d;', '👎')
+
 
 print("Do not close the terminal")
 keyboard.wait("Esc")
